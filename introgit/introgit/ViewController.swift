879ef8973adbacd06af3064757fcd1802655e1b0
Sugar Toast <taikisgame@gmail.com>
2019-07-27 15:41:44 +0900

    initial commit

---
+++ introgit/introgit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  introgit
+//
+//  Created by Sugar Toast on 2019/07/27.
+//  Copyright © 2019 Sugar Toast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
